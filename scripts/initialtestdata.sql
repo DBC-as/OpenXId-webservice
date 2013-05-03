202230b2bd302b0ea2a7d154934453b239b49e0c
Rasmus Jensen <git-email@solsort.dk>
2013-05-03 14:46:02 +0200

    moved repository to github.com/DBCDK/OpenXId-webservice and left a README with link to the new url

--- scripts/initialtestdata.sql
+++
@@ -1,9 +0,0 @@
-DELETE FROM oxid_ids;
-INSERT INTO oxid_ids (idtype, idvalue, recordid, clusterid) VALUES (0, '254321', 5, 5);
-INSERT INTO oxid_ids (idtype, idvalue, recordid, clusterid) VALUES (1, '87092',  5, 5);
-INSERT INTO oxid_ids (idtype, idvalue, recordid, clusterid) VALUES (1, '54987',  6, 5);
-INSERT INTO oxid_ids (idtype, idvalue, recordid, clusterid) VALUES (2, '453667', 2, 2);
-INSERT INTO oxid_ids (idtype, idvalue, recordid, clusterid) VALUES (3, '4555',   3, 2);
-INSERT INTO oxid_ids (idtype, idvalue, recordid, clusterid) VALUES (1, '666',    4, 5);
-INSERT INTO oxid_ids (idtype, idvalue, recordid, clusterid) VALUES (3, '222',    4, 5);
-INSERT INTO oxid_ids (idtype, idvalue, recordid, clusterid) VALUES (0, '5777',   6, 45);
